ca0548d3b4d5eb7ff3108d8f9667fa1de3fa78ba
TThaan <tthaan@gmx.de>
2020-09-21 12:20:02 +0200

    added Google LogIn
    only works in non-debugging mode (visual studio)

--- SignInAndUp_IR_Solution/SignInAndUp_IR/Program.cs
+++ SignInAndUp_IR_Solution/SignInAndUp_IR/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Hosting;
 /// (2) "show password" option
 /// (3) individualize Error page
 /// (4) ta: textContent in EmailService
-/// 
+/// (5) move input labels into the input field??
 /// btw-tasks
 /// answer on SO regarding: asp.net core identity "Invalid login attempt"
 /// </summary>
