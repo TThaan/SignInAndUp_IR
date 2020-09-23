a5215bdf075193a8e1961313c48600b3d3d80cf6
TThaan <tthaan@gmx.de>
2020-09-23 14:29:52 +0200

    changed Manage-Layout and highlightTab function

--- SignInAndUp_IR_Solution/SignInAndUp_IR/wwwroot/js/site.js
+++ SignInAndUp_IR_Solution/SignInAndUp_IR/wwwroot/js/site.js
@@ -1,10 +1,23 @@
-﻿//function highlightActiveTab()
-//{
-//    var current = location.pathname;
-//    $('.nav-tabs li a').each(function () {
-//        var $this = $(this);
-//        if (current.indexOf($this.attr('href')) !== -1) {
-//            $this.addClass('active');
-//        }
-//    })
-//}
+﻿function highlightActiveTab()
+{
+    var nothingActive = true;
+    var current = location.pathname;
+
+    $('.nav-tabs li a').each(function ()
+    {
+        let $this = $(this);
+        let value = $this.attr('href');
+        let index = current.indexOf(value);
+        if (index !== -1)
+        {
+            $this.addClass('active');
+            nothingActive = false;
+            //return;
+        }
+    })
+    if (nothingActive)
+    {
+        let defaultTab = $('.nav-tabs li a').first();
+        defaultTab.addClass('active');
+    }
+}
